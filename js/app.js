794d3ee0a78cf63fb4a88f6694bdda78113d0ca8
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2021-10-28 10:41:25 +0200

    [ADD] - Home Portfolio Animation

--- js/app.js
+++ js/app.js
@@ -29,3 +29,56 @@ if (document.querySelector('.home-reviews .swiper')) {
 
 /* #GSAP Animations
 ================================================== */
+gsap.registerPlugin(ScrollTrigger);
+
+// Portfolio
+if (document.querySelector('.home-portfolio')) {
+  const allItems = document.querySelectorAll('.home-portfolio .portfolio li');
+
+  if (window.innerWidth > 1024) {
+    allItems.forEach((item, index) => {
+      if (index == 1 || index == 4 || index == 7 || index == 10) {
+        gsap.to(item, {
+          yPercent: 50,
+          ease: "none",
+          scrollTrigger: {
+            trigger: ".home-portfolio .portfolio",
+            scrub: true
+          }, 
+        });
+      } else {
+        gsap.to(item, {
+          yPercent: -50,
+          ease: "none",
+          scrollTrigger: {
+            trigger: ".home-portfolio .portfolio",
+            scrub: true
+          }, 
+        });
+      }
+    });
+  } else {
+    allItems.forEach((item, index) => {
+      if (index % 2 == 0) {
+        gsap.to(item, {
+          yPercent: -25,
+          ease: "none",
+          scrollTrigger: {
+            trigger: ".home-portfolio .portfolio",
+            scrub: true
+          }, 
+        });
+      } else {
+        gsap.to(item, {
+          yPercent: 25,
+          ease: "none",
+          scrollTrigger: {
+            trigger: ".home-portfolio .portfolio",
+            scrub: true
+          }, 
+        });
+      }
+    });
+  }
+  
+}
