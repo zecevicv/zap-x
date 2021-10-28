fd70bd03ee2b79e6513ee3b15e2f5f1cd4296fb5
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2021-10-28 11:33:05 +0200

    [ADD] - Homepage - GSAP Animations

--- js/app.js
+++ js/app.js
@@ -31,6 +31,67 @@ if (document.querySelector('.home-reviews .swiper')) {
 ================================================== */
 gsap.registerPlugin(ScrollTrigger);
 
+// Banner
+if (document.querySelector('.home-banner')) {
+  let tl = gsap.timeline();
+
+  tl.from('.home-banner .img', {
+    opacity: 0,
+    duration: 1,
+    delay: 1,
+    ease: Power1.easeInOut,
+    scrollTrigger: {
+      trigger: ".home-banner",
+      start: "0 80%",
+    }, 
+  });
+}
+
+// Service
+if (document.querySelector('.home-services')) {
+  gsap.from('.home-services .swiper-slide', {
+    yPercent: -25,
+    opacity: 0,
+    duration: .5,
+    ease: Power1.easeInOut,
+    stagger:{ each: 0.15, from: 'right' },
+    scrollTrigger: {
+      trigger: ".home-services .swiper",
+      start: "0 80%",
+    }, 
+  });
+}
+
+// Reviews
+if (document.querySelector('.home-reviews')) {
+  gsap.from('.home-reviews .swiper-slide', {
+    yPercent: -25,
+    opacity: 0,
+    duration: .4,
+    ease: Power1.easeInOut,
+    stagger:{ each: 0.15, from: 'right' },
+    scrollTrigger: {
+      trigger: ".home-reviews",
+      start: "0 80%",
+    }, 
+  });
+}
+
+// Steps
+if (document.querySelector('.home-steps')) {
+  gsap.from('.home-steps .step', {
+    scale: .75,
+    opacity: 0,
+    duration: .5,
+    ease: Power1.easeInOut,
+    stagger:{ each: 0.25, from: 'right' },
+    scrollTrigger: {
+      trigger: ".home-steps",
+      start: "0 80%",
+    }, 
+  });
+}
+
 // Portfolio
 if (document.querySelector('.home-portfolio')) {
   const allItems = document.querySelectorAll('.home-portfolio .portfolio li');
